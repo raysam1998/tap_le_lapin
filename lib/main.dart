03bbc827fea37d51e91360347e35727aec7b77ad
rayaneBouzroura <2111752@cegepmontpetit.ca>
2024-06-09 20:09:34 -0400

    update : better animations

--- lib/main.dart
+++ lib/main.dart
@@ -40,9 +40,13 @@ class _MyHomePageState extends State<MyHomePage> {
   int found = 0;
   bool showRabbit = true;
   bool _isHovered = false;
-  int _hoveredIndex = -1;
+  int _hoveredIndex = -1; //hold
 
-  void _randomizeLeLapinou(){
+  double _rabbitOpacity = 0.0;
+  List<bool> clickedContainers =
+      List.generate(9, (index) => false); //tracks clicked
+
+  void _randomizeLeLapinou() {
     rabbitPosition = Random().nextInt(9);
   }
 
@@ -69,59 +73,80 @@ class _MyHomePageState extends State<MyHomePage> {
           //display the button grid
           Expanded(
               child: Container(
-                width: 300,
-                height: 300,
-                child: GridView.builder(
-                            //crossAxis of 3 to have a 3 x 3 grid
-                            gridDelegate:
+            width: 300,
+            height: 300,
+            child: GridView.builder(
+              //crossAxis of 3 to have a 3 x 3 grid
+              gridDelegate:
                   SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-                            itemCount: 9,
-                            itemBuilder: (context, index) {
+              itemCount: 9,
+              itemBuilder: (context, index) {
                 //return gesture detector w the stylyzed aspect as return
                 return MouseRegion(
                   onEnter: (event) {
                     setState(() {
                       _hoveredIndex = index;
                     });
-                    
                   },
                   onExit: (event) {
                     setState(() {
                       _hoveredIndex = -1;
                     });
-                    
                   },
                   child: GestureDetector(
                     onTap: () {
                       setState(() {
                         if (index == rabbitPosition) {
                           found++;
-                          _randomizeLeLapinou();
+                          //snackbar
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text('Ta trouver le lapinou woohoo !'),
+                            duration: Duration(seconds: 1),
+                            backgroundColor: Colors.green,
+                          ));
+                          //animate le lapinou
+                          _rabbitOpacity = 1.0;
+                          //buncha code to animate the rabbit easing in n out
+                          Future.delayed(Duration(seconds: 1), () {
+                            //hide rabbit icon after 1s
+                            _rabbitOpacity = 0.0;
+                            _randomizeLeLapinou();
+                            //reset list
+                            clickedContainers =
+                                List.generate(9, (index) => false);
+                          });
                         } else {
+                          clickedContainers[index] = true;
+                          print(clickedContainers);
                           attempts++;
                         }
                       });
                     },
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      margin: EdgeInsets.all(8.0),
-                      color: index == _hoveredIndex ?  Colors.red[100] : Colors.blue[100],
-                      //if showRabbit bool is true and index on rabbit show the rabbit
-                      child: Center(
-                        child: showRabbit && index == rabbitPosition
-                            ? Icon(
-                                Icons.pets,
-                                size: 10.0,
-                                color: Colors.lightGreenAccent,
-                              )
-                            : null,
+                    child: Center(
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        margin: EdgeInsets.all(8.0),
+                        color: index == _hoveredIndex
+                            ? Colors.red[100] //if we hovering then red
+                            : clickedContainers[
+                                    index] //else if clickedContainer
+                                ? Colors.yellow[100] //then yellow
+                                : Colors.blue[100], // else standard blue
+                        //if showRabbit bool is true and index on rabbit show the rabbit
+                        child: Center(
+                          child: showRabbit && index == rabbitPosition
+                              ? RabbitIcon(opacity: _rabbitOpacity)//if we are allowed to show the rabbit then
+                              : clickedContainers[index]
+                                  ? sadEmoji()
+                                  : null,
+                        ),
                       ),
                     ),
                   ),
                 );
-                            },
-                          ),
-              )),
+              },
+            ),
+          )),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(children: [
@@ -133,27 +158,59 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                onPressed :() {
+                onPressed: () {
                   setState(() {
                     attempts = 0;
                     found = 0;
                   });
                 },
                 child: Text("Reset counter"),
-                ),
-                ElevatedButton(
-                onPressed :() {
+              ),
+              ElevatedButton(
+                onPressed: () {
                   setState(() {
                     showRabbit = !showRabbit;
                   });
                 },
-                child: Text("toggle rabbit"),
-                ),
+                child: Text("show solution"),
+              ),
             ],
           ),
           SizedBox(height: 80),
         ],
       ),
+    );
+  }
+}
+
+class RabbitIcon extends StatelessWidget {
+  final double opacity;
+
+  RabbitIcon({required this.opacity});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return AnimatedOpacity(
+      opacity: opacity,
+      duration: Duration(milliseconds: 300),
+      child: Icon(
+        Icons.pets,
+        size: 50,
+        color: Colors.lightGreen,
+      ),
+    );
+  }
+}
+
+class sadEmoji extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Icon(
+      Icons.sentiment_dissatisfied,
+      size: 50.0,
+      color: Colors.red,
     );
   }
 }
